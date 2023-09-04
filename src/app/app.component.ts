f78973cf82e702351425cb1b07430e4c2509ddd6
akmal-kamarudin <akmal.kamarudin19@gmail.com>
2023-09-04 23:05:51 +0800

    lesson2: Create HomeComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template: `<h1>Hello world!</h1>`,
+  standalone: true,
+  imports: [HomeComponent],
+  template: `<main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+    <h1>Hello world!</h1>`,
 })
 export class AppComponent {
   title = 'home';
